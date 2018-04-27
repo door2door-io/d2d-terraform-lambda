e723949dbb3aa84f5fa919fcda8e9513e149b09c
Ivan Lopez <ivan@askai.net>
2018-04-27 12:13:41 +0200

    Remove missing outputs

--- output.tf
+++ output.tf
@@ -26,14 +26,6 @@ output "last_modified" {
   value = "${coalesce(join(",", aws_lambda_function.function.*.last_modified), join(",", aws_lambda_function.function_vpc.*.last_modified))}"
 }
 
-output "kms_key_arn" {
-  value = "${coalesce(join(",", aws_lambda_function.function.*.kms_key_arn), join(",", aws_lambda_function.function_vpc.*.kms_key_arn))}"
-}
-
 output "source_code_hash" {
   value = "${coalesce(join(",", aws_lambda_function.function.*.source_code_hash), join(",", aws_lambda_function.function_vpc.*.source_code_hash))}"
-}
-
-output "source_code_size" {
-  value = "${coalesce(join(",", aws_lambda_function.function.*.source_code_size), join(",", aws_lambda_function.function_vpc.*.source_code_size))}"
 }
