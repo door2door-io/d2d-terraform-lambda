6a9666e1e8cb55dc37e52414f0ec81deaf6ecc12
Ivan Lopez <ivan@askai.net>
2018-04-27 15:38:21 +0200

    Support no environment variables at all (required for Lambda@Edge)

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "lambda_arn" {
-  value = "${coalesce(join(",", aws_lambda_function.function.*.arn), join(",", aws_lambda_function.function_vpc.*.arn))}"
+  value = "${coalesce(join(",", aws_lambda_function.function.*.arn), join(",", aws_lambda_function.function_vpc.*.arn), join(",", aws_lambda_function.function_noenv.*.arn), join(",", aws_lambda_function.function_vpc_noenv.*.arn))}"
 }
 
 output "role_arn" {
@@ -11,21 +11,21 @@ output "role_name" {
 }
 
 output "qualified_arn" {
-  value = "${coalesce(join(",", aws_lambda_function.function.*.qualified_arn), join(",", aws_lambda_function.function_vpc.*.qualified_arn))}"
+  value = "${coalesce(join(",", aws_lambda_function.function.*.qualified_arn), join(",", aws_lambda_function.function_vpc.*.qualified_arn), join(",", aws_lambda_function.function_noenv.*.arn), join(",", aws_lambda_function.function_vpc_noenv.*.arn))}"
 }
 
 output "invoke_arn" {
-  value = "${coalesce(join(",", aws_lambda_function.function.*.invoke_arn), join(",", aws_lambda_function.function_vpc.*.invoke_arn))}"
+  value = "${coalesce(join(",", aws_lambda_function.function.*.invoke_arn), join(",", aws_lambda_function.function_vpc.*.invoke_arn), join(",", aws_lambda_function.function_noenv.*.arn), join(",", aws_lambda_function.function_vpc_noenv.*.arn))}"
 }
 
 output "version" {
-  value = "${coalesce(join(",", aws_lambda_function.function.*.version), join(",", aws_lambda_function.function_vpc.*.version))}"
+  value = "${coalesce(join(",", aws_lambda_function.function.*.version), join(",", aws_lambda_function.function_vpc.*.version), join(",", aws_lambda_function.function_noenv.*.arn), join(",", aws_lambda_function.function_vpc_noenv.*.arn))}"
 }
 
 output "last_modified" {
-  value = "${coalesce(join(",", aws_lambda_function.function.*.last_modified), join(",", aws_lambda_function.function_vpc.*.last_modified))}"
+  value = "${coalesce(join(",", aws_lambda_function.function.*.last_modified), join(",", aws_lambda_function.function_vpc.*.last_modified), join(",", aws_lambda_function.function_noenv.*.arn), join(",", aws_lambda_function.function_vpc_noenv.*.arn))}"
 }
 
 output "source_code_hash" {
-  value = "${coalesce(join(",", aws_lambda_function.function.*.source_code_hash), join(",", aws_lambda_function.function_vpc.*.source_code_hash))}"
+  value = "${coalesce(join(",", aws_lambda_function.function.*.source_code_hash), join(",", aws_lambda_function.function_vpc.*.source_code_hash), join(",", aws_lambda_function.function_noenv.*.arn), join(",", aws_lambda_function.function_vpc_noenv.*.arn))}"
 }
